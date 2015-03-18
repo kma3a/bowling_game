d5bf4b3ad18815ed2d566c50ee4942fdfaae0ef5
Kelsey Ando <kma3a@virginia.edu>
2015-03-18 16:46:09 -0400

    adding the function getRoll

--- bowlingGame.js
+++ bowlingGame.js
@@ -4,7 +4,10 @@ var BowlingGame = {
 	currentTotal: 0,
 	roll: function(endNum){
 		return Math.floor(Math.random() * endNum + 1);
-	}
+	},
+	getRoll: function() {
+		this.frame.push(this.roll(10));
+	}	
 
 
 }
