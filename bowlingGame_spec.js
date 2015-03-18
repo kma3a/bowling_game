a3af3ce51fea8e0c11db16e09fd4c05276a4e100
Kelsey Ando <kma3a@virginia.edu>
2015-03-18 14:24:44 -0400

    adding test to check that the frame is created

---
+++ bowlingGame_spec.js
@@ -0,0 +1,9 @@
+var bowlingGame = require('./bowlingGame.js');
+
+describe("Bowling Game", function() {
+
+	it("Starts with an empty array",function(){
+		expect(BowlingGame.frame).toEqual([]);	
+	});
+
+})
